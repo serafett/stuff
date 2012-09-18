e57f9a870df1146ff7a9d1dd24c502ce52e6be41
Peter Veentjer <alarmnummer@gmail.com>
2012-09-18 21:02:47 +0300

    Lots of work

---
+++ content/src/chapter-distributed-executor/execution-callback/src/main/java/MasterMember.java
@@ -0,0 +1,23 @@
+import com.hazelcast.core.*;
+import java.util.concurrent.*;
+public class MasterMember {
+    public static void main(String[] args){
+        HazelcastInstance hzInstance = Hazelcast.newHazelcastInstance(null);
+        ExecutorService es = hzInstance.getExecutorService();
+        DistributedTask<Long> task = new DistributedTask<Long>(new FibonacciCallable(10));
+        ExecutionCallback<Long> executionCallback = new ExecutionCallback<Long>() {
+            public void done(Future<Long> future) {
+                try {
+                    if (!future.isCancelled()) {
+                        System.out.println("Fibonacci calculation result = " + future.get());
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        task.setExecutionCallback(executionCallback);
+        es.execute(task);
+        System.out.println("Finished");
+    }
+}
