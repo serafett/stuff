9e1d0d48c0e487395fd06ac18f9839da8192627c
Peter Veentjer <alarmnummer@gmail.com>
2012-10-01 09:15:02 +0300

    Lots of work

---
+++ content/src/chapter-learning-basics/wildcard-configuration/src/main/java/WildcardMember.java
@@ -0,0 +1,15 @@
+import com.hazelcast.core.*;
+import java.util.Map;
+public class WildcardMember {
+    public static void main(String[] args) throws Exception {
+        HazelcastInstance hzInstance = Hazelcast.newHazelcastInstance(null);
+        Map map1 = hzInstance.getMap("testmap1");
+        Map map2 = hzInstance.getMap("testmap2");
+
+        map1.put("foo","foo");
+        for(;;){
+            System.out.println("size:"+map1.size());
+            Thread.sleep(1000);
+        }
+    }
+}
