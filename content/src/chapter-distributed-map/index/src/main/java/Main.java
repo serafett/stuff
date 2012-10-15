3c052aa79db912fc8862af6dcdaa6cc5fbe7cb51
Peter Veentjer <peter.veentjer@cloudsoft.com>
2012-10-15 09:31:34 +0300

    More work on index

--- content/src/chapter-distributed-map/index/src/main/java/Main.java
+++ content/src/chapter-distributed-map/index/src/main/java/Main.java
@@ -1,15 +1,40 @@
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
+import com.hazelcast.query.Predicate;
+import com.hazelcast.query.Predicates;
+
+import java.util.Random;
 
 public class Main {
+    public final static int MAP_SIZE = 100000;
+    public final static int SEARCH_COUNT = 1000;
+
+    private static final String[] names = new String[]{"Jacob", "Sophia", "Mason", "Isabella",
+            "William", "Emma","Jayden", "Olivia", "Noah", "Ava", "Michael", "Emily",
+            "Ethan", "Abigail",  "Alexander", "Madison","Aiden", "Mia","Daniel", "Chloe"};
+
     public static void main(String[] args) {
-        HazelcastInstance hzInstance  = Hazelcast.newHazelcastInstance(null);
-        IMap personMap = hzInstance.getMap("persons");
+        HazelcastInstance hzInstance = Hazelcast.newHazelcastInstance(null);
+        IMap<String, Person> personMap = hzInstance.getMap("pesrsons");
 
-        Person person = new Person("foo",true,10);
-        person.address.street="foo";
+        System.out.println("Generating testdata");
+        Random random = new Random();
+        for (int k = 0; k < MAP_SIZE; k++) {
+            String name = names[random.nextInt(names.length)];
+            personMap.put(""+k, new Person(name));
+        }
+        System.out.println("Generating testdata completed");
 
-        personMap.put("foo",person);
+        System.out.println("Starting benchmark");
+        long startMs = System.currentTimeMillis();
+        for(int k=0;k< SEARCH_COUNT;k++){
+           Predicate predicate = Predicates.equal(Predicates.get("name"),names[random.nextInt(names.length)]);
+           personMap.values(predicate);
+        }
+        long durationMs = System.currentTimeMillis()-startMs;
+        double performance = (SEARCH_COUNT*1000d)/durationMs;
+        System.out.println("Total duration: "+durationMs+" ms");
+        System.out.println("Performance: "+performance+" searches per second");
     }
 }
