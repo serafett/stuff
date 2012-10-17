e877017455aec035986bd25ef6e84070600a3eb9
Peter Veentjer <peter.veentjer@cloudsoft.com>
2012-10-17 09:33:05 +0300

    Work on the MapStore

---
+++ content/src/chapter-distributed-map/mapstore/src/main/java/PersonMapStore.java
@@ -0,0 +1,110 @@
+import com.hazelcast.core.MapStore;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static java.lang.String.format;
+
+public class PersonMapStore implements MapStore<Long, Person> {
+
+    private final Connection con;
+
+    public PersonMapStore() {
+        try {
+            con = DriverManager.getConnection("jdbc:hsqldb:mydatabase", "SA", "");
+            con.createStatement().executeUpdate(
+                    "create table if not exists person (id bigint, name varchar(45))");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("PersonMapStore created");
+    }
+
+    @Override
+    public void delete(Long key) {
+        try {
+            con.createStatement().executeUpdate(format("delete from person where id = %s", key));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void store(Long key, Person value) {
+        try {
+            con.createStatement().executeUpdate(format("insert into person values(%s,'%s')", key, value.name));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void storeAll(Map<Long, Person> map) {
+        for (Map.Entry<Long, Person> entry : map.entrySet()) {
+            store(entry.getKey(), entry.getValue());
+        }
+    }
+
+    @Override
+    public void deleteAll(Collection<Long> keys) {
+       for(Long key: keys){
+           delete(key);
+       }
+    }
+
+    @Override
+    public Person load(Long key) {
+        try {
+            ResultSet resultSet = con.createStatement().executeQuery(
+                    format("select name from person where id =%s", key));
+            try {
+                if (!resultSet.next()) {
+                    return null;
+                }
+                String name = resultSet.getString(1);
+                return new Person(name);
+            } finally {
+                resultSet.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Map<Long, Person> loadAll(Collection<Long> keys) {
+        Map<Long, Person> result = new HashMap<Long, Person>();
+        for (Long key : keys) {
+            result.put(key, load(key));
+        }
+        return result;
+    }
+
+    @Override
+    public Set<Long> loadAllKeys() {
+        Set<Long> keys = new HashSet<Long>();
+        try {
+            ResultSet resultSet = con.createStatement().executeQuery(
+                    "select id from person");
+            try {
+
+                while (resultSet.next()) {
+                    keys.add(resultSet.getLong(1));
+                }
+
+                return keys;
+            } finally {
+                resultSet.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
