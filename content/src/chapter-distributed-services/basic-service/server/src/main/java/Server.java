00bdeb33e7723e59b42df774046c6443ecc5e58e
Peter Veentjer <peter.veentjer@cloudsoft.com>
2012-10-19 17:10:43 +0300

    More work on the distributed services

--- content/src/chapter-distributed-services/basic-service/server/src/main/java/Server.java
+++ content/src/chapter-distributed-services/basic-service/server/src/main/java/Server.java
@@ -1,16 +1,25 @@
-import com.hazelblast.server.Slice;
-import com.hazelblast.server.SliceServer;
-import com.hazelblast.server.pojoslice.PojoSlice;
-import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.HazelcastInstance;
-
+import com.hazelblast.server.*;
+import com.hazelblast.server.pojoslice.*;
+import com.hazelcast.core.*;
 public class Server {
-
     public static void main(String[] args){
         HazelcastInstance hzInstance = Hazelcast.newHazelcastInstance(null);
         Pojo pojo = new Pojo(hzInstance);
         Slice slice = new PojoSlice(pojo);
         SliceServer sliceServer = new SliceServer(slice);
         sliceServer.start();
+    }
+    public static class Pojo implements HazelcastInstanceProvider {
+        @Exposed
+        public final EmployeeService employeeService;
+        private final HazelcastInstance hzInstance;
+
+        public Pojo(HazelcastInstance hzInstance) {
+            this.hzInstance = hzInstance;
+            this.employeeService = new EmployeeServiceImpl(hzInstance);
+        }
+        public HazelcastInstance getHazelcastInstance() {
+            return hzInstance;
+        }
     }
 }
