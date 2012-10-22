3353c450645b825d7cbb59bede4765b1663e696e
Peter Veentjer <peter.veentjer@cloudsoft.com>
2012-10-22 08:50:12 +0300

    More editing

--- content/src/chapter-distributed-collections/blockingqueue/src/main/java/ProducerMember.java
+++ content/src/chapter-distributed-collections/blockingqueue/src/main/java/ProducerMember.java
@@ -4,7 +4,7 @@ public class ProducerMember {
     public static void main(String[] args) throws Exception {
         HazelcastInstance hzInstance = Hazelcast.newHazelcastInstance(null);
         BlockingQueue<Integer> queue = hzInstance.getQueue("queue");
-        for (int k = 1; k < 1000; k++) {
+        for (int k = 1; k < 100; k++) {
             queue.put(k);
             System.out.println("Producing: " + k);
             Thread.sleep(1000);
