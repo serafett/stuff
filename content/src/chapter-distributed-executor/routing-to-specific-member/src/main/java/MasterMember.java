9e5b3ce4c50baa4ae2f55bd36c7cc1b2bd6c2d0d
Peter Veentjer <peter.veentjer@cloudsoft.com>
2012-10-24 08:57:19 +0300

    More work on executors

--- content/src/chapter-distributed-executor/routing-to-specific-member/src/main/java/MasterMember.java
+++ content/src/chapter-distributed-executor/routing-to-specific-member/src/main/java/MasterMember.java
@@ -1,4 +1,6 @@
 import com.hazelcast.core.*;
+import com.hazelcast.partition.PartitionService;
+
 import java.util.concurrent.*;
 public class MasterMember {
     public static void main(String[] args) {
@@ -6,8 +8,7 @@ public class MasterMember {
         ExecutorService executorService = hzInstance.getExecutorService();
         for (Member member : hzInstance.getCluster().getMembers()) {
             EchoTask task = new EchoTask("echo" + member.getInetSocketAddress());
-            Callable callable = new DistributedTask.DistributedRunnableAdapterImpl(task, null);
-            executorService.execute(new DistributedTask(callable, member));
+            executorService.execute(new DistributedTask(task, null, member));
         }
     }
 }
--- content/src/chapter-distributed-executor/routing-to-subset-of-members/src/main/java/MasterMember.java
+++ content/src/chapter-distributed-executor/routing-to-subset-of-members/src/main/java/MasterMember.java
@@ -5,18 +5,13 @@ public class MasterMember {
     public static void main(String[] args) throws Exception {
         HazelcastInstance hzInstance = Hazelcast.newHazelcastInstance(null);
         Map<String,Integer> map = hzInstance.getMap("map");
-        //insert dummy data
         for (int k = 0; k < 5; k++)
             map.put(UUID.randomUUID().toString(), 1);
-        //fork the tasks.
         Set<Member> members = hzInstance.getCluster().getMembers();
         MultiTask<Integer> task = new MultiTask<Integer>(new SumTask(), members);
-        ExecutorService executorService = hzInstance.getExecutorService();
-        executorService.execute(task);
-        //join the results.
+        hzInstance.getExecutorService().execute(task);
         Collection<Integer> results = task.get();
-        int x = 0;
-        for (Integer i : results) x += i;
+        int x = 0; for (Integer i : results) x += i;
         System.out.println("Result: " + x);
     }
 }
