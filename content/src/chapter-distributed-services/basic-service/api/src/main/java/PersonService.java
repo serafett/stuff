3a7cb3abeba84e1fd00c5b7259a31737b6536567
peter.veentjer <alarmnummer@gmail.com>
2013-01-20 11:54:20 +0200

    Removed the distributed services example code

--- content/src/chapter-distributed-services/basic-service/api/src/main/java/PersonService.java
+++
@@ -1,8 +0,0 @@
-import com.hazelblast.client.annotations.*;
-@DistributedService
-public interface PersonService {
-    @Partitioned
-    void create(@PartitionKey String id, String name);
-    @Partitioned
-    Person get(@PartitionKey String id);
-}
