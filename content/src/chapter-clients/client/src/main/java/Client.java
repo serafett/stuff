733a2d6c80ab08c61ed71f263b3705815a53895c
Peter Veentjer <alarmnummer@gmail.com>
2013-02-12 10:44:20 +0200

    * more client improvements

--- content/src/chapter-clients/client/src/main/java/Client.java
+++ content/src/chapter-clients/client/src/main/java/Client.java
@@ -1,16 +1,14 @@
 import com.hazelcast.client.*;
+import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.Executor;
-import java.util.concurrent.locks.Lock;
 
-public class NativeClient {
+public class Client {
     public static void main(String[] args) throws Exception {
         ClientConfig clientConfig = new ClientConfig().addAddress("127.0.0.1");
         HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
-        System.out.println(client.getCluster().getMembers());
         BlockingQueue<String> queue = client.getQueue("queue");
         queue.put("Hello!");
-        System.out.println("Message send by native member!");
+        System.out.println("Message send by client!");
     }
 }
