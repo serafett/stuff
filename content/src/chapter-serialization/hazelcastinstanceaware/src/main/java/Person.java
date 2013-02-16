869d10c9e4ac443b143c1a1a074e53201439df38
Peter Veentjer <alarmnummer@gmail.com>
2013-02-16 10:39:30 +0200

    * moved hazelcastinstanceaware section to serialization from the executor * some minor updates on the learningthebasics

---
+++ content/src/chapter-serialization/hazelcastinstanceaware/src/main/java/Person.java
@@ -0,0 +1,24 @@
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.HazelcastInstanceAware;
+
+import java.io.Serializable;
+
+public class Person implements Serializable,HazelcastInstanceAware {
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private transient  HazelcastInstance hz;
+    public Person(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setHazelcastInstance(HazelcastInstance hz) {
+        this.hz = hz;
+        System.out.println("hazelcastInstance set");
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Person(name=%s)",name);
+    }
+}
